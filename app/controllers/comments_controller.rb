1f56debeaa4fa7ff1e13ca4ee21a80acb7869244
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-04-10 15:20:34 +0000

    fixed the bugs with delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
-  before_action :is_an_authorized_user, only: [:destroy, :create]
-  before_action :ensure_current_user_is_author, only: [:edit, :update]
+  before_action :is_an_authorized_user, only: [:create]
+  before_action :ensure_current_user_is_author, only: [:edit, :update, :destroy]
 
   # GET /comments or /comments.json
   def index
@@ -23,6 +23,7 @@ class CommentsController < ApplicationController
 
   # POST /comments or /comments.json
   def create
+    puts "creating xyz"
     @comment = Comment.new(comment_params)
     @comment.author = current_user
 
@@ -52,6 +53,7 @@ class CommentsController < ApplicationController
 
   # DELETE /comments/1 or /comments/1.json
   def destroy
+    puts "in destroy"
     @comment.destroy
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
